dbf7cf83308bb648b6f4d80fc6d4308abee33fd5
Arsenii Machekhin <arsenium2@gmail.com>
2023-07-14 18:07:10 +0300

    create new component 'FirstScreenCompilation'

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import './MainPage.css';
+import FirstScreenCompilation from '../../components/FirstScreenCompilation/FirstScreenCompilation';
 
 export default function MainPage() {
 	return (
 		<main className="main-page" id="main-page">
+      <FirstScreenCompilation />
 			<h1 className="main-page__title">Main</h1>
 			<Link to="/sign-in">Войти</Link>
 			<Link to="/sign-up">Зарегистрироваться</Link>
 		</main>
 	);
+	
 }
