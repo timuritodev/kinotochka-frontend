7aa812ea7a38a1ffe0839003ba2227bc5a0c8dd6
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-07-18 02:46:08 +0300

    добавление меню профиля и и главного меню

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,47 @@
 import { FC } from 'react';
+import { useState } from 'react';
 import './Header.css';
 import logo from '../../images/logo.svg';
+import adjustments from '../../images/adjustments.svg';
 import search from '../../images/search.svg';
 import Account from '../Account/Account';
 import { Link } from 'react-router-dom';
 
 const Header: FC = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const setNavOpen = () => {
+		setIsOpen(true);
+	};
+
+	const setNavClose = () => {
+		setIsOpen(false);
+	};
 	return (
 		<header className="header" id="header">
-			<Link to="/">
-				<img className="header__logo" alt="лого" src={logo} />
-			</Link>
+			<img
+				className="header__logo"
+				alt="лого"
+				src={logo}
+				onMouseOver={setNavOpen}
+			/>
+			<nav
+				className={`header__content ${isOpen && 'header__content_open'}`}
+				onMouseOver={setNavOpen}
+				onMouseOut={setNavClose}
+			>
+				<ul className="header__list" onMouseOver={setNavOpen}>
+					<Link to="/" className="header__content-link">
+						Главная
+					</Link>
+					<Link to="/favorites" className="header__content-link">
+						Наши подборки
+					</Link>
+					<Link to="/will-see" className="header__content-link">
+						Фильмы по жанрам
+					</Link>
+				</ul>
+			</nav>
 			<form className="header__search">
 				<input
 					className="header__search-input"
@@ -18,27 +49,25 @@ const Header: FC = () => {
 					name="name"
 					type="text"
 					placeholder="Какой фильм вы хотите найти?"
-					// value={values.name || ''}
-					// onChange={handleChange}
-					// disabled={isLoading}
 				/>
-				<button className="header__search-button" type="submit">
+				<button className="header__search-button">
+					<img
+						className="header__search-button_search"
+						src={adjustments}
+						alt="Кнопка расширенного поиска"
+					/>
+				</button>
+				<button className="header__search-button">
 					<img
 						className="header__search-button_search"
 						src={search}
 						alt="Кнопка поиска"
-						// disabled={!isValid}
 					/>
 				</button>
 			</form>
 			<Account
 			// isLoggedIn={true}
 			/>
-			<Link to="/rated-films">Оцененные фильмы</Link>
-			<Link to="/will-see">Буду смотреть</Link>
-			<Link to="/favorites">Избранные</Link>
-			<Link to="/collections">Подборки</Link>
-			<Link to="/movie-page">Страница фильма</Link>
 		</header>
 	);
 };
