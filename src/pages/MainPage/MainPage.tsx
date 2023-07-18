9fa5ba9c907e0db2e40a2771376a8377bc3bac2f
Arsenii Machekhin <arsenium2@gmail.com>
2023-07-18 18:33:41 +0300

    style: Подложил фон под хедер на главной странице

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import './MainPage.css';
 import FirstScreenCompilation from '../../components/FirstScreenCompilation/FirstScreenCompilation';
 import Slider, { SliderTypes } from 'src/components/Slider/Slider';
 import { GENRES } from 'src/utils/constants';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { getFilmsApi } from '../../services/redux/slices/films/films';
 
 export default function MainPage() {
+
+ const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(getFilmsApi());
+	}, []);
+
+	const films = useAppSelector((state) => state.films.films);
+
 	return (
 		<main className="main-page" id="main-page">
-			<FirstScreenCompilation />
+			<FirstScreenCompilation film={films[0]} />
 			<h1 className="main-page__title">Main</h1>
 			<Link to="/sign-in">Войти</Link>
 			<Link to="/sign-up">Зарегистрироваться</Link>
