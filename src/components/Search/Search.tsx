baa0d02aa0b6289a78fb41246c54c279a3d4f20e
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-07-19 23:58:04 +0300

    добавление поиска

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,15 @@
-import { FC } from 'react';
-import { useState, useEffect } from 'react';
+// import { FC } from 'react';
+// import { useState, useEffect } from 'react';
 import './Search.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Search = ({ isOpenSearch }: { isOpenSearch: boolean }) => {
-	const [isOpen, setIsOpen] = useState(false);
 
-	return <section className={`search ${isOpen && 'search_open'}`}></section>;
+	return <section className={`searchAll ${isOpenSearch && 'search_open'}`}>
+
+
+		
+	</section>;
 };
 
 export default Search;
