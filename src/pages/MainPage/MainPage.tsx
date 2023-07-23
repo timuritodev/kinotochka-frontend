8bc67c1298513da5a9ac7baaefd19ea65e9e75b3
Anton Lyscov <anton.lyscov@yandex.ru>
2023-07-23 12:32:34 +0300

    add Slider genres

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import './MainPage.css';
 import FirstScreenCompilation from '../../components/FirstScreenCompilation/FirstScreenCompilation';
 import Slider from 'src/components/Slider/Slider';
-import { GENRES } from 'src/utils/constants';
+import { GENRES, GENRESv2 } from 'src/utils/constants';
 import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
 import { getFilmsApi } from '../../services/redux/slices/films/films';
 import { SliderTypes } from '../../types/Slider.types';
@@ -23,28 +23,17 @@ export default function MainPage() {
 	return (
 		<main className="main-page" id="main-page">
 			<FirstScreenCompilation film={films[0]} />
-			<h1 className="main-page__title">Main</h1>
-			<Link to="/sign-in">Войти</Link>
-			<Link to="/sign-up">Зарегистрироваться</Link>
-			<br />
-			{/* <Slider contentType={SliderTypes.genresRow} content={GENRES} /> */}
 			<div className="main-page_slick-slider">
-				<SlickSlider type={SlickSliderTypes.oscar} />
+				<SlickSlider type={SlickSliderTypes.news} />
 			</div>
 			<div className="main-page_slick-slider">
-				<SlickSlider type={SlickSliderTypes.specialforyou} />
+				<SlickSlider type={SlickSliderTypes.oscar} />
 			</div>
 			<div className="main-page_slick-slider">
 				<SlickSlider type={SlickSliderTypes.blackwhite} />
 			</div>
 			<div className="main-page_slick-slider">
-				<SlickSlider type={SlickSliderTypes.news} />
-			</div>
-			<div className="main-page_slick-slider">
-				<SlickSlider type={SlickSliderTypes.similar} />
-			</div>
-			<div className="main-page_slick-slider">
-				<SlickSliderGenres content={GENRES} />
+				<SlickSliderGenres content={GENRESv2} />
 			</div>
 		</main>
 	);
