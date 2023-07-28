e2bdab5af52f9ca0c94563562922524c3019d6dd
allvinnik <allvinnik@mail.ru>
2023-07-28 12:53:20 +0300

    Добавление массива на страницу, гриды

--- src/pages/PreferencesPage/PreferencesPage.tsx
+++ src/pages/PreferencesPage/PreferencesPage.tsx
@@ -1,13 +1,20 @@
 import './PreferencesPage.css';
 import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
 import { FC } from 'react';
+import { GENRES } from '../../utils/constants' ;
 
 const PreferencesPage: FC = () => {
 	const dispatch = useAppDispatch();
 
 	return (
 		<section className="preferencespage">
-			<div className="preferencespage__container"></div>
+			<ul className="preferencespage__container">
+			{GENRES.map((genre) => {
+            return (
+              <li className='preferencespage__list'>{genre}</li>
+            );
+          })}
+			</ul>
 		</section>
 	);
 };
