54108f6d56edb3b9cae352c7afc975e60f78977a
allvinnik <allvinnik@mail.ru>
2023-07-29 14:31:12 +0300

    checkbox

--- src/pages/PreferencesPage/PreferencesPage.tsx
+++ src/pages/PreferencesPage/PreferencesPage.tsx
@@ -1,24 +1,35 @@
 import './PreferencesPage.css';
 import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import CheckboxMain from '../../components/CheckboxMain/CheckboxMain';
 import { GENRES } from '../../utils/constants';
-import  SaveButton  from '../../components/SaveButton/SaveButton';
+import SaveButton from '../../components/SaveButton/SaveButton';
+
 
 const PreferencesPage: FC = () => {
-	
+
+	const [checked, setChecked] = useState(false);
+
+	const handleChange = () => {
+		setChecked(!checked);
+	};
 
 	return (
 		<section className="preferencespage">
-			<h2 className='title'>Мои предпочтения</h2>
-			<h3 className='subtitle'>Избранные жанры</h3>
+			<h2 className="title">Мои предпочтения</h2>
+			<h3 className="subtitle">Избранные жанры</h3>
 			<ul className="preferencespage__container">
-				{GENRES.map((genre) => {
-					return <li className="preferencespage__list">{genre}</li>;
-				})}
+				{GENRES.map((genre) => (
+					
+			<li className="preferencespage__list">
+			<CheckboxMain text={genre} onChange={handleChange} />
+			</li>		
+				))}
 			</ul>
-			<SaveButton buttonText='Сохранить' handleButtonClick={undefined} />
+			<SaveButton buttonText="Сохранить" handleButtonClick={undefined} />
 		</section>
 	);
 };
 
 export default PreferencesPage;
+
