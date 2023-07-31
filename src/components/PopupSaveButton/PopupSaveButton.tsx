f59a831803cecc54a31e65d34870c5bba1cf883f
allvinnik <allvinnik@mail.ru>
2023-07-31 21:10:59 +0300

    попап готов

--- src/components/PopupSaveButton/PopupSaveButton.tsx
+++ src/components/PopupSaveButton/PopupSaveButton.tsx
@@ -14,25 +14,26 @@ const PopupSaveButton: FC<IButton> = ({ handleButtonClick, isPopupOpen }) => {
 					? 'popupsavebutton-overlay popupsavebutton_open'
 					: 'popupsavebutton'
 			}
-		><div className='popupsavebutton__container'>
-			<p className='popupsavebutton__title'>Сохранить изменения</p>
-			<p className='popupsavebutton__subtitle'>Изменения сохранены</p>
-			<button
-				className="button__main"
-				// disabled
-				type="button"
-				onClick={handleButtonClick}
-			>
-				На Главную
-			</button>
-			<button
-				className="button__close"
-				// disabled
-				type="button"
-				onClick={handleButtonClick}
-			>
-				Закрыть
-			</button>
+		>
+			<div className="popupsavebutton__container">
+				<p className="popupsavebutton__title">Сохранить изменения</p>
+				<p className="popupsavebutton__subtitle">Изменения сохранены</p>
+				<button
+					className="button__main"
+					// disabled
+					type="button"
+					onClick={handleButtonClick}
+				>
+					На Главную
+				</button>
+				<button
+					className="button__close"
+					// disabled
+					type="button"
+					onClick={handleButtonClick}
+				>
+					Закрыть
+				</button>
 			</div>
 		</section>
 	);
