807e93b64a5f62ec754630b088019a79dcf16f0d
luciD <sosalnet@yandex.ru>
2023-08-04 23:11:31 +0300

    fixed movie-cards and added click to one for test

--- src/components/FilmCardWidth255/FilmCard.tsx
+++ src/components/FilmCardWidth255/FilmCard.tsx
@@ -2,16 +2,21 @@ import './FilmCard.css';
 import { RatedElement } from 'src/components/RatedElement/RatedElement';
 import { BookmarkSmall } from 'src/components/Bookmark_small/Bookmark_small';
 import { IMovieCard } from 'src/types/MovieCard.types';
+import { useNavigate } from 'react-router-dom';
 
 export const FilmCard = ({ film }: { film: IMovieCard }) => {
+	const navigate = useNavigate();
+
+	const handleImgClick = () => {
+		navigate('/movie-page');
+	};
 	return (
 		<section key={film.id} className="flanks_card">
-			<img className="flanks_card-img" src={film.v_picture} alt="" />
+			<img className="flanks_card-img" src={film.v_picture} alt=""  onClick={handleImgClick}/>
 			<div className="bookmark-small">{<BookmarkSmall id={film.id} />}</div>
 			<h4 className="flanks_card-title">{film.title}</h4>
-			<p className="flanks_card-subtitle">{`${film.genres.join(', ')} • ${
-				film.year
-			}`}</p>
+			<p className="flanks_card-subtitle">{`${film.genres.join(', ')} • ${film.year
+				}`}</p>
 			<RatedElement
 				imdb={film.rating.rate_imdb}
 				kinopoisk={film.rating.rate_kinopoisk}
