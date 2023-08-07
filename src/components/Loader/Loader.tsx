5ecb18731ad570771482e1d5e463c38cbeb830c1
luciD <sosalnet@yandex.ru>
2023-08-07 19:13:07 +0300

    loader in progress

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,13 @@
 import './Loader.css';
 import { FC } from 'react';
-import loader1 from '../../images/loader1.svg'
-import loader2 from '../../images/loader2.svg'
+import loader1 from '../../images/loader12.svg';
+import loader2 from '../../images/loader2.svg';
 
 export const Loader: FC = () => {
-    return (
-        <div className="loader">
-            <div className='loader__img' />
-            <div className='loader__img_plus' />
-        </div>
-    )
+  return (
+    <div className="loader">
+      <div className="loader__img" />
+      <div className="loader__img_plus" />
+    </div>
+  );
 };
-
