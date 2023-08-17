0aacf8b786ef3a16c16eff6a2ed842b5fb98ab28
luciD <sosalnet@yandex.ru>
2023-08-18 02:31:41 +0300

    fixed cards and added click

--- src/components/SlickSliderMini/SlickSliderMini.tsx
+++ src/components/SlickSliderMini/SlickSliderMini.tsx
@@ -1,18 +1,18 @@
-import './SlickSlider.css';
+import '../SlickSlider/SlickSlider.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { FilmCard } from '../FilmCardWidth255/FilmCard';
 import { FC } from 'react';
 import { ICompilationsTwo } from 'src/types/Compilations.types';
+import { FilmCardSmall } from '../FilmCardWidth180/FilmCardSmall';
 
-export const SlickSlider: FC<ICompilationsTwo> = ({ title, movies }) => {
+export const SlickSliderMini: FC<ICompilationsTwo> = ({ title, movies }) => {
 
 	const settings = {
 		dots: false,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 4,
+		slidesToShow: 5,
 		slidesToScroll: 4,
 		arrows: true,
 	}
@@ -21,7 +21,7 @@ export const SlickSlider: FC<ICompilationsTwo> = ({ title, movies }) => {
 		<div className="slick-slider_container">
 			<h1 className="slick-slider_title">{title}</h1>
 			<Slider {...settings} className="slick-slider">
-				{movies.map((item) => <FilmCard film={item} />)}
+				{movies.map((item) => <FilmCardSmall film={item} />)}
 			</Slider>
 		</div>
 	);
