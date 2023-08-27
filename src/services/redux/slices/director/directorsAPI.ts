abdc01d21bf28022fa2752a07e4263ed04c64892
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-08-27 19:36:51 +0300

    feat: выпадающий список режиссеров в расширенном поиске

---
+++ src/services/redux/slices/director/directorsAPI.ts
@@ -0,0 +1,24 @@
+import { IDirectors } from 'src/types/Directors.types';
+import { API_BASE_URL } from 'src/utils/constants';
+
+
+const checkRes = (res: Response) => {
+	if (res.ok) {
+		return res.json();
+	} else {
+		return Promise.reject(res);
+	}
+};
+
+const fetchData = (url: string) => {
+	return fetch(url, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((res) => checkRes(res));
+};
+
+export const fetchActors = (): Promise<Array<IDirectors>> => {
+	return fetchData(`${API_BASE_URL}/directors/`);
+};
