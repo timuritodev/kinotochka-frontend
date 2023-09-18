90802c0e8123b67f03411d7c0cfd9a63eb82a653
YuliaDuk <yule444ka1234@mail.ru>
2023-09-18 15:19:46 +0300

    feat: создана страница 500 по роуту /500

---
+++ src/pages/ServerErrorPage/ServerErrorPage.tsx
@@ -0,0 +1,10 @@
+import './ServerErrorPage.css';
+import React, { useEffect } from 'react';
+
+export default function ServerErrorPage() {
+    return (
+    <main className='server-error'>
+        
+    </main>
+    )
+}
