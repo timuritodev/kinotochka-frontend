d99be50d55857fb74d2e6c4370ced500f0861c0b
YuliaDuk <yule444ka1234@mail.ru>
2023-09-18 22:39:20 +0300

    feat: добавлена 500 страница

--- src/pages/ServerErrorPage/ServerErrorPage.tsx
+++ src/pages/ServerErrorPage/ServerErrorPage.tsx
@@ -2,5 +2,12 @@ import './ServerErrorPage.css';
 import React, { useEffect } from 'react';
 
 export default function ServerErrorPage() {
-	return <main className="server-error"></main>;
+	return (
+		<main className="server-error">
+			<h1 className='server-error__title'>500</h1>
+			<h2 className='server-error__subtitle'>Ошибка сервера</h2>
+			<p className='server-error__description'>На сервере произошла непредвиденная ошибка.</p>
+			<p className='server-error__description'>Мы уже исправляем ее. Пожалуйста, попробуйте обновить страницу позже</p>
+		</main>
+	);
 }
