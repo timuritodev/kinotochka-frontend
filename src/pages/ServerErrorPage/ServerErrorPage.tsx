538051c98f4832debad975f5cbc0944cd2d99fba
YuliaDuk <yule444ka1234@mail.ru>
2023-09-19 14:09:18 +0300

    style: код отформатирован

--- src/pages/ServerErrorPage/ServerErrorPage.tsx
+++ src/pages/ServerErrorPage/ServerErrorPage.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 export default function ServerErrorPage() {
 	return (
 		<main className="server-error">
-			<div className='server-error__container'>
+			<div className="server-error__container">
 				<h1 className="server-error__title">500</h1>
 				<h2 className="server-error__subtitle">Ошибка сервера</h2>
 				<p className="server-error__description">
