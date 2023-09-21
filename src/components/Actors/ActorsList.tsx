62849f694db73c410507867ac3f18d1c0b80af2c
YuliaDuk <yule444ka1234@mail.ru>
2023-09-21 18:54:09 +0300

    fix: баг 281-изменен текст на В главных ролях

--- src/components/Actors/ActorsList.tsx
+++ src/components/Actors/ActorsList.tsx
@@ -6,7 +6,7 @@ import { ActorsListProps } from 'src/types/Rating.types';
 const ActorsList: FC<ActorsListProps> = ({ actors }) => {
 	return (
 		<section className="actorslist">
-			<h1 className="actorslist__title">Актеры</h1>
+			<h1 className="actorslist__title">В главных ролях</h1>
 			<div className="actorslist__container">
 				<p className="actorslist__text">
 					{actors.map((item) => item).join(', ')}
