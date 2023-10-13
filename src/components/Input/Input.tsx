3cdf20b0236ae3f5ce6d8b590ee5b9b9fd5ea9dc
YuliaDuk <yule444ka1234@mail.ru>
2023-10-13 12:31:22 +0300

    код отформатирован

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -20,7 +20,7 @@ const Input: FC<IInput> = ({
 	defaultValue,
 }) => {
 	const [isPasswordHidden, setIsPasswordHidden] = useState(true);
-	const {width} = useResize();
+	const { width } = useResize();
 	useEffect(() => {
 		setIsPasswordHidden(true);
 	}, []);
@@ -37,108 +37,112 @@ const Input: FC<IInput> = ({
 			: inputType;
 
 	return (
-		<> {width >=1000 ? (
-			<div className="input__container input__container_type_desktop">
-				<div
-					className={
-						location.pathname === '/profile'
-							? 'input__hints input__hints_type_profile'
-							: 'input__hints'
-					}
-				>
-					{labelText ? (
-						<label
-							className={`input__label input__label_color_${
-								color !== 'white' ? 'white' : 'black'
-							}`}
-							htmlFor={inputType}
-						>
-							{labelText}
-						</label>
-					) : null}
-					{error ? <span className="input__error">{error}</span> : null}
-				</div>
-				<input
-					{...validation}
-					onChange={
-						onChange
-							? onChange
-							: (e) => {
-									validation.onChange(e);
-							  }
-					}
-					className={`input__field input__field_type_${inputType} input__field_color_${color} ${
-						error ? 'input__field_invalid' : ''
-					}`}
-					type={inputTextType}
-					name={inputType}
-					id={inputType}
-					readOnly={readOnly}
-					max={max}
-					defaultValue={defaultValue}
-					maxLength={inputTextType === 'date' ? 8 : undefined}
-					value={readOnly && value ? value : undefined}
-				/>
-				{showPasswordButton ? (
-					<button
-						className="input__button"
-						type="button"
-						onClick={togglePassword}
+		<>
+			{' '}
+			{width >= 1000 ? (
+				<div className="input__container input__container_type_desktop">
+					<div
+						className={
+							location.pathname === '/profile'
+								? 'input__hints input__hints_type_profile'
+								: 'input__hints'
+						}
+					>
+						{labelText ? (
+							<label
+								className={`input__label input__label_color_${
+									color !== 'white' ? 'white' : 'black'
+								}`}
+								htmlFor={inputType}
+							>
+								{labelText}
+							</label>
+						) : null}
+						{error ? <span className="input__error">{error}</span> : null}
+					</div>
+					<input
+						{...validation}
+						onChange={
+							onChange
+								? onChange
+								: (e) => {
+										validation.onChange(e);
+								  }
+						}
+						className={`input__field input__field_type_${inputType} input__field_color_${color} ${
+							error ? 'input__field_invalid' : ''
+						}`}
+						type={inputTextType}
+						name={inputType}
+						id={inputType}
+						readOnly={readOnly}
+						max={max}
+						defaultValue={defaultValue}
+						maxLength={inputTextType === 'date' ? 8 : undefined}
+						value={readOnly && value ? value : undefined}
 					/>
-				) : null}
-			</div>) : (
-			<div className="input__container input__container_type_mobile">
-				<div
-					className={
-						location.pathname === '/profile'
-							? 'input__hints input__hints_type_profile'
-							: 'input__hints'
-					}
-				>
-					{labelText ? (
-						<label
-							className={`input__label input__label_color_${
-								color !== 'white' ? 'white' : 'black'
-							}`}
-							htmlFor={inputType}
-						>
-							{labelText}
-						</label>
+					{showPasswordButton ? (
+						<button
+							className="input__button"
+							type="button"
+							onClick={togglePassword}
+						/>
 					) : null}
 				</div>
-				<input
-					{...validation}
-					onChange={
-						onChange
-							? onChange
-							: (e) => {
-									validation.onChange(e);
-							  }
-					}
-					className={`input__field input__field_type_${inputType} input__field_color_${color} ${
-						error ? 'input__field_invalid' : ''
-					}`}
-					type={inputTextType}
-					name={inputType}
-					id={inputType}
-					readOnly={readOnly}
-					max={max}
-					defaultValue={defaultValue}
-					maxLength={inputTextType === 'date' ? 8 : undefined}
-					value={readOnly && value ? value : undefined}
-				/>
-				<div className="input__error-container">
-					{' '}
-					{error ? <span className="input__error">{error}</span> : null}
-				</div>
-				{showPasswordButton ? (
-					<button
-						className="input__button"
-						type="button"
-						onClick={togglePassword}
+			) : (
+				<div className="input__container input__container_type_mobile">
+					<div
+						className={
+							location.pathname === '/profile'
+								? 'input__hints input__hints_type_profile'
+								: 'input__hints'
+						}
+					>
+						{labelText ? (
+							<label
+								className={`input__label input__label_color_${
+									color !== 'white' ? 'white' : 'black'
+								}`}
+								htmlFor={inputType}
+							>
+								{labelText}
+							</label>
+						) : null}
+					</div>
+					<input
+						{...validation}
+						onChange={
+							onChange
+								? onChange
+								: (e) => {
+										validation.onChange(e);
+								  }
+						}
+						className={`input__field input__field_type_${inputType} input__field_color_${color} ${
+							error ? 'input__field_invalid' : ''
+						}`}
+						type={inputTextType}
+						name={inputType}
+						id={inputType}
+						readOnly={readOnly}
+						max={max}
+						defaultValue={defaultValue}
+						maxLength={inputTextType === 'date' ? 8 : undefined}
+						value={readOnly && value ? value : undefined}
 					/>
-				) : null}
-			</div> )}
+					<div className="input__error-container">
+						{' '}
+						{error ? <span className="input__error">{error}</span> : null}
+					</div>
+					{showPasswordButton ? (
+						<button
+							className="input__button"
+							type="button"
+							onClick={togglePassword}
+						/>
+					) : null}
+				</div>
+			)}
 		</>
 	);
 };
